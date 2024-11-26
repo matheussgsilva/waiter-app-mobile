0e71fa37248ad193d8ae682ce75c44a25c269e2a
Matheus Silva <matheussgsilva@hotmail.com>
2024-11-26 20:53:48 -0300

    Create the Header, Categories an Menu components

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,10 +1,26 @@
-import { Text } from "../components/Text";
-import { Container } from "./styles";
+import { Categories } from "../components/Categories";
+import { Header } from "../components/Header";
+import { Menu } from "../components/Menu";
+import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from "./styles";
 
 export function Main() {
   return (
-    <Container>
-      <Text>Main renderizou</Text>
-    </Container>
+    <>
+      <Container>
+        <Header />
+
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+      </Container>
+
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
+    </>
   )
 }
