4e2ecf8dc656af50876b6a41ea5d93f401008fb8
Matheus Silva <matheussgsilva@hotmail.com>
2024-12-05 21:40:36 -0300

    Create handleSelectCategory function

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { FlatList } from "react-native";
 import { categories } from "../../mocks/categories";
 import { Text } from "../Text";
 import { Category, Icon } from "./styles";
 
 export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+
+    setSelectedCategory(category);
+  }
+
   return (
     <FlatList
       horizontal
@@ -11,15 +20,23 @@ export function Categories() {
       contentContainerStyle={{ paddingRight: 24 }}
       data={categories}
       keyExtractor={category => category._id}
-      renderItem={({ item: category }) => (
-        <Category>
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
+
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
           <Icon>
-            <Text>{category.icon}</Text>
+            <Text opacity={isSelected? 1 : 0.5}>
+              {category.icon}
+            </Text>
           </Icon>
 
-          <Text size={14} weight="600">{category.name}</Text>
+          <Text size={14} weight="600" opacity={isSelected? 1 : 0.5}>
+            {category.name}
+          </Text>
         </Category>
-      )}
+        )
+      }}
     />
   );
 }
