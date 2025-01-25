f22544444777698e34e31a4c62bde1c1785f8503
Matheus Silva <matheussgsilva@hotmail.com>
2025-01-25 20:01:23 -0300

    Change product receive as a prop

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,5 @@
 import { FlatList } from "react-native";
 
-import { products } from "../../mocks/products";
 import { Text } from "../Text";
 
 import React, { useState } from "react";
@@ -12,9 +11,10 @@ import { AddToCartButton, ProductContainer, ProductImage, Separator } from "./st
 
 interface MenuProps {
   onAddToCart: (product: Product) => void;
+  products: Product[];
 }
 
-export function Menu({ onAddToCart}: MenuProps) {
+export function Menu({ onAddToCart, products }: MenuProps) {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState<null | Product>(null);
 
